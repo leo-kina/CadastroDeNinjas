e6abc906a5c53c29dd3f96b889cde53abc260fbf
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-12 20:14:25 -0300

    fix: Colocando nome das Colunas na classe

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -19,12 +19,18 @@ public class NinjaModel {
 //anotacao id serve para falar q o atributo logo em
 //baixo dele vai ser um id
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY) //gerar automaticamente o ID
-   private Long id;
-   private String nome;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //gerar automaticamente o ID
+    @Column(name = "id") //para nomear as colunas
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
    @Column(unique = true) //coluna precisa ser unica (ou seja o email nao vai repetir)
    private String email;
-   private int idade;
+
+    @Column(name = "idade")
+    private int idade;
 
    @ManyToOne//um ninja tem uma unica missao
    @JoinColumn(name = "missoes_id") //Foreing Key ou chave estrangeira
