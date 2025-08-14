155054c215fd017c5c55a68c4916f204fb23eccb
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-14 15:39:37 -0300

    feat:Atualizacao Controller, com Mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -9,4 +9,32 @@ public class NinjaController {
     public String boasVindas(){
         return "Essa é minha primeria mensgaem nessa rota";
     }
+
+
+    //Adicionar ninja (Create)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+    //Mostrar todos os ninjas (Read)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Ninjas";
+    }
+    //Procurar ninja por ID (Create)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Ninjas";
+    }
+    //Alterar dados dos ninjas (Update)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId(){
+        return "Alterar Ninja por Id";
+    }
+    //Deletar Ninjas (Delete)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Deletar Ninja por id";
+    }
+    //Ou seja é o CRUD
 }
