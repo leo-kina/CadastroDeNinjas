11b4d82de9d2fd6f9147a01207f742dd80703838
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-16 13:53:40 -0300

    fix: trocando para DTO a camada de controller para service

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -2,23 +2,34 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/missoes")
 public class MissoesController {
+
+    private MissoesService missoesService;
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
     @GetMapping("/listar")
-    public String listarMissoes(){
-        return "Missoes listadas";
+    public List<MissoesDTO> listarMissoes(){
+       return missoesService.listarMissoes();
+    }
+    @GetMapping("/listar/{id}")
+    public MissoesDTO listarMissaoPorId(@PathVariable Long id){
+        return missoesService.listarMissoesPorId(id);
     }
     @PostMapping("/criar")
-    public String criarMissoes(){
-        return "Missao criada";
+    public MissoesDTO criarMissoes(@RequestBody MissoesDTO missoesDTO){
+        return missoesService.criarMissao(missoesDTO);
     }
-    @PutMapping("/alterar")
-    public String alterarMissao(){
-        return "Missao alterada";
+    @PutMapping("/alterar/{id}")
+    public MissoesDTO alterarMissaoPorId(@PathVariable Long id, @RequestBody MissoesDTO missoesDTO){
+        return missoesService.attualizarMissoes(id,missoesDTO);
     }
-    @DeleteMapping("/deletar")
-    public String deletarMissao(){
-        return "Missao deletada";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarMissao(@PathVariable Long id){
+        missoesService.deletarMissoes(id);
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,56 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaDTO;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaMapper;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class MissoesService {
+    @Autowired
+    private MissoesMapper missoesMapper;
+
+    @Autowired
+    private MissoesRepository missoesRepository;
+
+    public List<MissoesDTO> listarMissoes(){
+        List<MissoesModel> missoes = missoesRepository.findAll();
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public MissoesDTO listarMissoesPorId(Long id){
+        Optional<MissoesModel> missoesId = missoesRepository.findById(id);
+        return missoesId.map(missoesMapper::map).orElse(null);
+    }
+
+    public MissoesDTO criarMissao(@RequestBody MissoesDTO missoesDTO){
+        MissoesModel missoes = missoesMapper.map(missoesDTO);
+        missoes = missoesRepository.save(missoes);
+        return missoesMapper.map(missoes);
+    }
+
+    public void deletarMissoes (Long id){
+        missoesRepository.deleteById(id);
+    }
+    public MissoesDTO attualizarMissoes(Long id, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missaoExistente = missoesRepository.findById(id);
+        if(missaoExistente.isPresent()){
+            MissoesModel missaoAtualizada = missoesMapper.map(missoesDTO);
+            missaoAtualizada.setId(id);
+            MissoesModel missaoSalva = missoesRepository.save(missaoAtualizada);
+            return missoesMapper.map(missaoSalva);
+        }
+        return null;
+    }
+
+
 }
