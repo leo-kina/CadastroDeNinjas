607dd5244c5185a8f8d6b232b865d2cc58fd8ace
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-27 21:03:15 -0300

    refactor: mudando db h2 para postgres

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -8,25 +8,24 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-//JPA = Java Persistence API
-//Entity ele transforma a classe em uma entidade do db
+
 @Entity
 @Table(name = "tb_cadastro")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class NinjaModel {
-//anotacao id serve para falar q o atributo logo em
-//baixo dele vai ser um id
+
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //gerar automaticamente o ID
-    @Column(name = "id") //para nomear as colunas
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "nome")
     private String nome;
 
-   @Column(unique = true) //coluna precisa ser unica (ou seja o email nao vai repetir)
+   @Column(unique = true)
    private String email;
 
     @Column(name = "idade")
@@ -35,8 +34,8 @@ public class NinjaModel {
     @Column(name = "rank")
     private String rank;
 
-   @ManyToOne//um ninja tem uma unica missao
-   @JoinColumn(name = "missoes_id") //Foreing Key ou chave estrangeira
+   @ManyToOne
+   @JoinColumn(name = "missoes_id") //chave estrangeira
    private MissoesModel missoes;
 
 
